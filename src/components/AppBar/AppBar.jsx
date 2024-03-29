99a3b743cfba4daa3ce829cb28ec10c2b42a97be
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-03-29 15:14:11 +0100

    Updating nav menue

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,34 +1,45 @@
 import { NavLink } from "react-router-dom";
-// import clsx from 'clsx';
-import { Suspense } from 'react';
-import {  Outlet } from 'react-router-dom';
-import css from './AppBar.module.css';
+import clsx from "clsx";
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+
+import css from "./AppBar.module.css";
 
 const AppBar = () => {
-//   const buildLinkClass = ({ isActive }) => {
-//     return clsx(css.link, isActive && css.active);
-//   };
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
 
   return (
     <div>
-    <header>
-      <p className={css.logo}>
-        <span role="img" aria-label="computer icon">
-          💻
-        </span>{' '}
-        GoSearch Movies
-      </p>
-      <nav className={css.nav}>
-        <NavLink to='/' activeClassName={css.active}> Home</NavLink>
-        <NavLink to='/movies' activeClassName={css.active} > Movies</NavLink>
-      </nav>
-    </header>
-          <Suspense fallback={<div>Loading...</div>}>
+      <header>
+        <p className={css.logo}>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{" "}
+          GoSearch Movies
+        </p>
+        <nav className={css.nav}>
+          <NavLink
+            to="/"
+            activeClassName={css.active}
+            className={buildLinkClass}
+          >
+                Home
+          </NavLink>
+          <NavLink
+            to="/movies"
+            activeClassName={css.active}
+            className={buildLinkClass}
+          >
+                Movies
+          </NavLink>
+        </nav>
+      </header>
+      <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
-
     </div>
-    
   );
 };
 
